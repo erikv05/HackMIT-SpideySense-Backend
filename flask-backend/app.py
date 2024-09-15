025b76132a4f93fd11ec44178f2bb7b8cb5864a2
erikv05 <erikv05@uw.edu>
2024-09-15 07:32:08 -0400

    fix: update type checking in flask API and enable auth

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -8,9 +8,10 @@ CORS(app)
 model = joblib.load("flask-backend/WESAD_binary_xgboost.pkl")
 
 def is_float(string):
-    if string.replace(".", "").isnumeric():
+    try:
+        float(string)
         return True
-    else:
+    except ValueError:
         return False
 
 @app.route('/predict', methods=['POST'])
@@ -25,27 +26,35 @@ def predict():
         return {"error": "Missing y_acceleration parameter"}, 400
     if not "z_acceleration" in data:
         return {"error": "Missing z_acceleration parameter"}, 400
-    if not "temperature" in data:
+    if "temperature" not in data:
         temperature = 5
 
     # Confirm data is strings
     if not isinstance(data["heart_rate"], (str, float, int)):
+        print("1")
         return {"error": "Invalid heart_rate parameter: data type"}, 400
     if not isinstance(data["x_acceleration"], (str, float, int)):
-        return {"error": "Invalid heart_rate parameter"}, 400
+        print("2")
+        return {"error": "Invalid heart_rate parameter: data type"}, 400
     if not isinstance(data["y_acceleration"], (str, float, int)):
-        return {"error": "Invalid heart_rate parameter"}, 400
+        print("3")
+        return {"error": "Invalid heart_rate parameter: data type"}, 400
     if not isinstance(data["z_acceleration"], (str, float, int)):
-        return {"error": "Invalid heart_rate parameter"}, 400
+        print("4")
+        return {"error": "Invalid heart_rate parameter: data type"}, 400
     
     # Confirm data can be cast
-    if not is_float(str(data["heart_rate"])):
+    if not is_float(data["heart_rate"]):
+        print(1)
         return {"error": "Invalid heart_rate parameter"}, 400
-    if not is_float(str(data["x_acceleration"])):
+    if not is_float(data["x_acceleration"]):
+        print(2)
         return {"error": "Invalid x_acceleration parameter"}, 400
-    if not is_float(str(data["y_acceleration"])):
+    if not is_float(data["y_acceleration"]):
+        print(3)
         return {"error": "Invalid y_acceleration parameter"}, 400
-    if not is_float(str(data["z_acceleration"])):
+    if not is_float(data["z_acceleration"]):
+        print(4)
         return {"error": "Invalid z_acceleration parameter"}, 400
     if not isinstance(data, str) or not data["temperature"].isnumeric():
         temperature = 5
@@ -63,8 +72,6 @@ def predict():
     z_acceleration = float(data["z_acceleration"])
 
     prediction = model.predict_proba([[heart_rate, x_acceleration, y_acceleration, z_acceleration]])[0][1]
-    print(prediction)
-    print(proba_cutoff)
     print(prediction.item())
 
     return {"prediction": "normal" if prediction.item() < proba_cutoff else "anomaly"}
