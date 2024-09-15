48325f75f2f50c02f5a602a46a429f0a50ef9273
erikv05 <erikv05@uw.edu>
2024-09-14 22:05:47 -0400

    feat: finish backed

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import joblib
+
 app = Flask(__name__)
 CORS(app)
+model = joblib.load("WESAD_binary_xgboost.pkl")
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    data = request.json
+    if not "heart_rate" in data:
+        return {"error": "Missing heart_rate parameter"}, 400
+    if not "x_acceleration" in data:
+        return {"error": "Missing x_acceleration parameter"}, 400
+    if not "y_acceleration" in data:
+        return {"error": "Missing y_acceleration parameter"}, 400
+    if not "z_acceleration" in data:
+        return {"error": "Missing z_acceleration parameter"}, 400
+
+    return {"prediction": "normal" if model.predict([[data["heart_rate"], data["x_acceleration"], data["y_acceleration"], data["z_acceleration"]]])[0] == 0 else "anomaly"}
     
-    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
